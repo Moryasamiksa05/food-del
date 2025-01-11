2281b9c58b5532d31ae6e2c8b25cc08b6a929a96
Moryasamiksa05 <152077294+Moryasamiksa05@users.noreply.github.com>
2025-01-11 15:32:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/HOme/HOme'
+import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOder from './pages/PlaceOrder/PlaceOder'
 import Footer from './components/Footer/Footer'
