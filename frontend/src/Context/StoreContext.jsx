12f221af84fbe54d1595f74f64156da7f5abbfd3
Moryasamiksa05 <152077294+Moryasamiksa05@users.noreply.github.com>
2025-01-11 15:00:07 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider= (props) => {
 
 
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://food-del-1-0oij.onrender.com"
     const [token,setToken ] = useState("");
     const [food_list,setFoodList] = useState([]);
 
